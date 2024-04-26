8da4345e2447e4460d5ead23b959d1706cc2e0b0
RuthDianaPurnamasari <ruthdianaps@gmail.com>
2024-04-26 15:12:54 +0700

    pra

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,11 @@
-import { get } from "https://bukulapak.github.io/api/process.js"; 
+import { get } from "https://bukulapak.github.io/api/process.js";
+import { setInner } from "https://bukulapak.github.io/element/process.js";
 let urlAPI = "https://ws-ruth2024-458a1e70d2e1.herokuapp.com/presensi";
 get(urlAPI,isiTablePresensi);
 function isiTablePresensi(results){
     console.log(results);
+    results.forEach(isiRow);
+}
+function isiRow(value){
+    console.log(value)
 }
