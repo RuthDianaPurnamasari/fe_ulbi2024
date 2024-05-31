2c71a45cb71a0047bb37d1389a9ebad114d97500
RuthDianaPurnamasari <ruthdianaps@gmail.com>
2024-05-31 22:26:00 +0700

    ya aaloh permudahlah

---
+++ js/config/url_post.js
@@ -0,0 +1,7 @@
+export let urlPOST = "https://ws-ruth2024-458a1e70d2e1.herokuapp.com/insert"
+
+export function AmbilResponse(result) {
+    console.log(result); //menampilkan response API pada console
+    alert(result.message); //menampilkan response API pada alert
+    window.location.reload(); //reload halaman setelah klik ok pada alert
+}
