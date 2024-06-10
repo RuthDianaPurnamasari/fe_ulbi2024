ff8ff6c30c901d9419f42456071910751b2c7866
RuthDianaPurnamasari <ruthdianaps@gmail.com>
2024-06-10 16:50:20 +0700

    pushh

---
+++ js/controller/delete.js
@@ -0,0 +1,17 @@
+function deleteData(IDHAPUS) {
+    var presensiId = IDHAPUS;
+    var target_url = "ws-ruth2024-458a1e70d2e1.herokuapp.com/delete/" + presensiId;
+
+    var requestOptions = {
+        method: 'DELETE',
+        redirect: 'follow'
+    };
+
+    fetch(target_url, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            alert(result.message);
+            location.reload();
+        })
+        .catch(error => console.log('Error:', error));
+}
